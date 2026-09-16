f6e1f9f0d232af6f4f3b1fb9ba59947753cc1601
NxReason <arsiddde@gmail.com>
2026-09-16 07:15:28 +0300

    nxorm: crud

--- libs/nxorm/src/nxorm/model.py
+++ libs/nxorm/src/nxorm/model.py
@@ -61,6 +61,66 @@ class Model:
         cursor.execute(query)
         self.conn.commit()
 
+    def create(self, data):
+        cursor = self.conn.cursor()
+        query = f'INSERT INTO {self._name} ({self._get_columns_str()}) VALUES ({self._get_values_placeholder()})'
+
+        values = []
+        for c, props in self.columns.items():
+            if props.primary:
+                continue
+
+            if c not in data:
+                if props.required and props.default == None:
+                    print(
+                        f'[ERROR:MODEL:CREATE] {c} not provided and no default value')
+                    return
+                data[c] = props.default
+
+            values.append(data[c])
+
+        cursor.execute(query, values)
+        self.conn.commit()
+
+    def read_all(self):
+        cursor = self.conn.cursor()
+        cursor.execute(f"SELECT * FROM {self._name}")
+        return cursor.fetchall()
+
+    def read_by_id(self, id):
+        cursor = self.conn.cursor()
+        cursor.execute(f'SELECT * FROM {self._name} WHERE id = ?', [id])
+        return cursor.fetchone()
+
+    def update(self, id, data):
+        cursor = self.conn.cursor()
+        query = f'UPDATE {self._name} SET\n'
+        values = []
+        for name, val in data.items():
+            query += f'{name} = ?,'
+            values.append(val)
+
+        query = query[:-1]
+        query += 'WHERE id = ?;'
+        values.append(id)
+
+        cursor.execute(query, values)
+
+    def delete(self, id):
+        cursor = self.conn.cursor()
+        query = f'DELETE FROM {self._name} WHERE id = ?'
+        cursor.execute(query, [id])
+
+    def _get_columns_str(self):
+        out = ', '.join(
+            [name for name, props in self.columns.items() if not props.primary])
+        return out
+
+    def _get_values_placeholder(self):
+        return ', '.join(
+            ['?' for name, props in self.columns.items() if not props.primary]
+        )
+
 
 def map_sql_type(t: type) -> str:
     match t:
