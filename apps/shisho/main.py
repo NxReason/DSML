e978162cc80102a652c7283aaf769a149c5ca412
NxReason <arsiddde@gmail.com>
2026-09-20 06:26:18 +0300

    shisho: re-added ignore words diff to output

--- apps/shisho/main.py
+++ apps/shisho/main.py
@@ -3,22 +3,35 @@ from src.input import filter_valid
 from src.output import cli, file
 from src.tokenizer import tokenize_file
 from src.parser import Parser
+from src.ignore.db import IgnoreDB
+
+args_map = {
+    "no_ignore": bool
+}
 
 
 def main():
-    args = nxcli.parse_args()
+    cmd = nxcli.parse_args(args_map)
 
-    inputs, errors = filter_valid(args.inputs)
+    inputs, errors = filter_valid(cmd.inputs)
     cli.inputs_summary(inputs, errors)
 
     tokens_dict = {}
     for input in inputs:
         tokens_dict[input] = tokenize_file(input)
 
+    # conditionally add ignore list
+    ignore_words = set()
+    if not cmd.no_ignore:
+        ignore_db = IgnoreDB()
+        ignore_words = set(w[1] for w in ignore_db.get_words())
+
     for path, tokens in tokens_dict.items():
         parser = Parser(tokens)
         parser.parse_core_words()
-        file.save_core_words(path, parser.core_words.keys())
+        print(len(ignore_words))
+        out_words = parser.core_words.keys() - ignore_words
+        file.save_core_words(path, out_words)
 
 
 if __name__ == "__main__":
